eadfe45873edddc1f65f4e43ee51cf8420cfa985
Alex Grover <alexgrover@microsoft.com>
2022-12-23 11:44:46 +0000

    PowerShell written

--- Get-ListsCSOM.ps1
+++ Get-ListsCSOM.ps1
@@ -4,13 +4,41 @@ Connect-PnPOnline -Url $url -Interactive
 
 $ctx = Get-PnPContext
 
-$lists = $ctx.Web.Lists
-
-$ctx.Load($lists)
+$web = $ctx.Web
+$ctx.Load($web)
 Invoke-PnPQuery
 
-#$lists
 
-Write-Host ""
-Write-Host "Found $($lists.count) lists in site"
-Write-Host ""
+$getListQuery = New-Object Microsoft.SharePoint.Client.GetListsParameters
+$getListQuery.RowLimit = 500
+
+## Will start at null or 0
+$position = $getListQuery.ListCollectionPosition
+
+do
+{
+    $getListQuery.ListCollectionPosition = $position
+    $listCol = $web.GetLists($getListQuery)
+    $ctx.Load($listCol)
+    Invoke-PnPQuery
+    $position = $listCol.ListCollectionPosition;
+    $allLists += $listCol
+    $allLists.Count
+
+    ## Subsequent pages
+    #$listCol | % { "Title: $($_.Title) ID: $($_.Id)"  }
+
+} while ($null -ne $position)
+
+## test
+$uniqueLists = $allLists | Select-Object -Property Id -Unique
+
+$uniqueLists.Count
+
+## May fail on sites with 60k+ lists
+$pnpLists = Get-PnPList
+
+if (($allLists.Count -eq $uniqueLists.Count) -and ($allLists.Count -eq $pnpLists.Count))
+{
+    "Passed"
+}
